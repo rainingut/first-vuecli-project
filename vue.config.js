14a826585367d2963eab10c0bf5f50fb9f3287d0
rainingut <keeprainingut@gmail.com>
2022-03-21 13:29:07 +0800

    更新dist 位置(github專案名/dist/)

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: '/dist/'
+  publicPath: '/first-vuecli-project/dist/'
 });
